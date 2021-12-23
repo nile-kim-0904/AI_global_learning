7684f82f1d076771d2421fe06fa101a8b91ba16c
nile <kimnamu0904@gmail.com>
2021-12-23 17:05:35 +0900

    day 1

---
+++ day_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # python
+# ### 기본 문법 1일차
+
+# In[9]:
+
+
+print('hello')
+print(10 * 20) 
+10 + 20
+
+
+# In[10]:
+
+
+import keyword
+keyword.kwlist
+
+
+# In[ ]:
+
+
+
+
